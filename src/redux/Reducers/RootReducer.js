2f22f3f3a598173edf63c05e63d02ec50ec9c681
FenilBorad7 <fenilborad789@gmail.com>
2022-05-26 10:51:46 +0530

    redux bug fixing

--- src/redux/Reducers/RootReducer.js
+++ src/redux/Reducers/RootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 
 // ** Reducers Imports
 import Reducer from './reducer';
+import swap from './Swap';
 
 const rootReducer = combineReducers({
   Reducer,
+  swap
 });
 
 export default rootReducer;
